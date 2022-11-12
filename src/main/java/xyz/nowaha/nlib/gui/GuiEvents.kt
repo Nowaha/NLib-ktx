e57bdecf6c870b5b3d2da65e6984cb54034c200d
Nowaha <noahverkaik@gmail.com>
2022-11-12 17:09:50 +0100

    1.0.1: Centralised GUI click listener

---
+++ src/main/java/xyz/nowaha/nlib/gui/GuiEvents.kt
@@ -0,0 +1,23 @@
+package xyz.nowaha.nlib.gui
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+
+interface GuiEvents : Listener {
+
+    val registeredGUIs: List<Gui<PluginInventoryHolder>>
+
+    @EventHandler(ignoreCancelled = true)
+    fun on(event: InventoryClickEvent) {
+        if (event.currentItem != null) {
+            registeredGUIs.firstOrNull { it.getTitle() == event.view.title }?.let {
+                event.isCancelled = true
+                if (event.clickedInventory == event.view.topInventory) {
+                    it.onClick(event)
+                }
+            }
+        }
+    }
+
+}
